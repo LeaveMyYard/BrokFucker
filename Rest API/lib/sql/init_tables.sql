86959cfacd747a482055b5c3517908d7a08d3664
leavemyyard <zhukovpavel2001@gmail.com>
2020-02-17 18:39:00 +0200

    Update init_tables.sql

--- Rest API/lib/sql/init_tables.sql
+++ Rest API/lib/sql/init_tables.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS Users (
     `id` INTEGER PRIMARY KEY AUTOINCREMENT,
     `email` TEXT NOT NULL,
     `password` TEXT NOT NULL,
+    `type` INTEGER NOT NULL,
     `reg_date` DATETIME NOT NULL,
     `name` TEXT,
-    `phone_number` TEXT
+    `phone_number` TEXT,
 );
