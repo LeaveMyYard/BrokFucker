f78dace9c62524a19ab1305748db75c018b47e47
Daniil <black3phoenix@gmail.com>
2020-02-28 16:41:27 +0200

    test v1.0.1

--- src/js/table_data.js
+++ src/js/table_data.js
@@ -1,22 +1,52 @@
 const URL = "http://localhost:5000/api/v1/";
 const lotTable = document.getElementById("lotTable");
 
-fetch(URL + "lots/approved")
-  .then(response => {
+function dateFix(date) {
+  let lotDate = new Date(date);
+  let day = lotDate.getDate();
+  let month = lotDate.getMonth();
+  let year = lotDate.getFullYear() % 100;
+  return `${day}/${month + 1}/${year}`;
+}
+
+const myFunc = async () => {
+  try {
+    const response = await fetch(URL + "lots/approved");
+
     if (!response.ok) {
       return Promise.reject(Error("Unsuccessfull response"));
     }
-    return response.json().then(({ results: lots }) => {
-      // lotTable.innerHTML = getLots(lots);
-      console.log(lots);
+
+    const result = await response.json();
+    console.log(result);
+    const lotArr = [];
+    result.forEach(item => {
+      item = `
+      <tr>
+      <td>${dateFix(item.date)}</td>
+      <td>${item.name}</td>
+      <td>${item.user}</td>
+      <td>${item.amount}</td>
+      <td>${item.currency}</td>
+      <td>${item.term}</td>
+      <td>${item.return_way}</td>
+      <td>${item.security}</td>
+      <td>${item.percentage}</td>
+      <td>${item.form}</td>
+      <td>${item.security_checked}</td>
+      <td>${item.guarantee_percentage}</td>
+      </tr>`;
+      lotArr.push(item);
     });
-  })
+    console.log(lotArr);
 
-  //onReject
-  .catch(err => {
-    console.log(err);
-  });
+    lotTable.innerHTML += lotArr.join("");
+  } catch (error) {
+    console.error(error);
+  }
+};
 
+myFunc();
 // function queryApi(endpoint) {
 //   return fetch(URL + endpoint).then(response => {
 //     return response.ok
