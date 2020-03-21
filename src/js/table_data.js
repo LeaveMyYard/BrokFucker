a52748e5ec56a396436461f8b25de7b690021e3d
Daniil <black3phoenix@gmail.com>
2020-03-21 19:44:12 +0200

    test prod 2

--- src/js/table_data.js
+++ src/js/table_data.js
@@ -1,4 +1,4 @@
-const URL = "http://localhost:5000/api/v1/";
+// const URL = "http://localhost:5000/api/v1/";
 const URL = `${window.location.host}/`;
 
 const lotTable = document.getElementById("lotTable");
