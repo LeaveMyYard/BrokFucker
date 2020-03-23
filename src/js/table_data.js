ccfd766961a06a646047549f54a52dfc217697bd
Daniil <black3phoenix@gmail.com>
2020-03-23 19:30:25 +0200

    index now displays all lots .

--- src/js/table_data.js
+++ src/js/table_data.js
@@ -44,7 +44,7 @@ onReady();
 
 const getLots = async () => {
   try {
-    const response = await fetch(URL + "lots/", {
+    const response = await fetch(URL + "lots", {
       method: "GET"
       // headers: { Authorization: `Basic ${encData()}` }
     });
