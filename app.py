c7e383dbf5f95f196ba184046bb62140edb6f51e
Павел Жуков <33721692+LeaveMyYard@users.noreply.github.com>
2020-04-07 18:44:09 +0300

    Some minor fixes and improvements

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def remove_1m():
     t.start()
 
 def remove_1h():
-    ip_weights['1s'] = {}
+    ip_weights['1h'] = {}
     t = Timer(3600.0, remove_1h)
     t.start()
 
@@ -79,11 +79,13 @@ def weighted(weight):
                     ban_list[request_ip] = [..., (ban_list[request_ip][1] if request_ip in ban_list else []) + [datetime.now()]]
                     ban_list[request_ip][1] = ban_cases = list(filter(lambda time: time > datetime.now() - timedelta(days=1), ban_list[request_ip][1]))
 
+                    for stats in ip_weights:
+                        stats[request_ip] = 0
+
                     ban_time = ban_durations[len(ban_cases) - 1]
                     ban_list[request_ip][0] = datetime.now() + ban_time
 
-                    logger.info(f"User {request_ip} was banned for {ban_time}")
-
+                    logger.warning(f"User {request_ip} was banned for {ban_time}")
                     raise APIExceptions.MaximumRequestsTimeout(int(ban_time.total_seconds()))
                 values[request_ip] += weight
 
