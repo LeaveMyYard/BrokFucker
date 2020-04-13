c2eb95dd986b5e7f9b735228d27f48c6d2b5a1fb
Daniil <black3phoenix@gmail.com>
2020-04-13 22:00:31 +0300

    added finished and sponsored lots + fixed some bugs

---
+++ admin/precache-manifest.e19d7452a5c5b6155139204c1b737d92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "318dda31c99f2775b146adc73dbdc650",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcd37b51aa9c602e82f0",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "46cd4dbe28e40392d90b",
+    "url": "/static/css/main.7e964538.chunk.css"
+  },
+  {
+    "revision": "dcd37b51aa9c602e82f0",
+    "url": "/static/js/2.e97a06a9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e97a06a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46cd4dbe28e40392d90b",
+    "url": "/static/js/main.a6c50b3e.chunk.js"
+  },
+  {
+    "revision": "58c1ee4396e707f235e3",
+    "url": "/static/js/runtime-main.c4cc651d.js"
+  }
+]);
